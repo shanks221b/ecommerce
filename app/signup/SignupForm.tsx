50ca0007ffbda1426cd6c3b33e4a52f51e233847
mbhasi <maanav.bhasin@data-axle.com>
2026-07-25 03:39:25 +0530

    Create SignupForm.tsx

---
+++ app/signup/SignupForm.tsx
@@ -0,0 +1,147 @@
+"use client";
+
+import NextLink from "next/link";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  Box,
+  Button,
+  Link,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+const Page = styled(Box)({
+  minHeight: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "#f5f5f5",
+});
+
+const Card = styled(Paper)({
+  width: 400,
+  padding: 32,
+});
+
+const Form = styled("form")({
+  display: "flex",
+  flexDirection: "column",
+  gap: 16,
+});
+
+const schema = yup.object({
+  firstName: yup
+    .string()
+    .required("First name is required"),
+
+  lastName: yup
+    .string()
+    .required("Last name is required"),
+
+  email: yup
+    .string()
+    .email("Invalid email")
+    .required("Email is required"),
+
+  password: yup
+    .string()
+    .min(6, "Minimum 6 characters")
+    .required("Password is required"),
+});
+
+type FormData = yup.InferType<typeof schema>;
+
+export default function SignupForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const submit = (data: FormData) => {
+    console.log(data);
+  };
+
+  return (
+    <Page>
+      <Card elevation={3}>
+        <Typography
+          variant="h4"
+          sx={{
+            textAlign: "center",
+            mb: 3,
+          }}
+        >
+          Sign Up
+        </Typography>
+
+        <Form onSubmit={handleSubmit(submit)}>
+          <TextField
+            label="First Name"
+            {...register("firstName")}
+            error={!!errors.firstName}
+            helperText={errors.firstName?.message}
+          />
+
+          <TextField
+            label="Last Name"
+            {...register("lastName")}
+            error={!!errors.lastName}
+            helperText={errors.lastName?.message}
+          />
+
+          <TextField
+            label="Email"
+            type="email"
+            {...register("email")}
+            error={!!errors.email}
+            helperText={errors.email?.message}
+          />
+
+          <TextField
+            label="Password"
+            type="password"
+            {...register("password")}
+            error={!!errors.password}
+            helperText={errors.password?.message}
+          />
+
+          <Button
+            variant="contained"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </Form>
+
+        <Typography
+          sx={{
+            textAlign: "center",
+            mt: 2,
+          }}
+        >
+          Already have an account?{" "}
+          <Link
+            component={NextLink}
+            href="/login"
+          >
+            Login
+          </Link>
+        </Typography>
+      </Card>
+    </Page>
+  );
+}
