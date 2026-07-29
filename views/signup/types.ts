a858e7f331db7981e737b2bec941aa0115f88865
mbhasi <Maanav.Bhasin@data-axle.com>
2026-07-29 11:47:07 +0530

    Add tests for login and signup forms

--- views/signup/types.ts
+++ views/signup/types.ts
@@ -1,11 +1,18 @@
 import * as yup from "yup";
 
 export const schema = yup.object({
-  firstName: yup.string().required("First name is required"),
+  firstName: yup
+    .string()
+    .required("First name is required"),
 
-  lastName: yup.string().required("Last name is required"),
+  lastName: yup
+    .string()
+    .required("Last name is required"),
 
-  email: yup.string().email("Invalid email").required("Email is required"),
+  email: yup
+    .string()
+    .email("Invalid email")
+    .required("Email is required"),
 
   password: yup
     .string()
